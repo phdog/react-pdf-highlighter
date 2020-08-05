d5330218af0d2d12bed29e5eb78fa0edc8759b9b
MaxK <max.kachanov@gmail.com>
2020-08-05 18:11:40 +0300

    moved index

---
+++ index.js
@@ -0,0 +1,7 @@
+import PdfHighlighter from "./dist/components/PdfHighlighter";
+import Tip from "./dist/components/Tip";
+import Highlight from "./dist/components/Highlight";
+import Popup from "./dist/components/Popup";
+import AreaHighlight from "./dist/components/AreaHighlight";
+import PdfLoader from "./dist/components/PdfLoader";
+export { PdfHighlighter, Tip, Highlight, Popup, AreaHighlight, PdfLoader };
